91dcb445d9383455e5fe687cec2ea888aafdfcca
dhyadav <dhanaji.yadav@mmltd.co.za>
2024-08-11 13:39:33 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/animation_5_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animation_5_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class AnimatedCirclesScreen extends StatefulWidget {
+  @override
+  _AnimatedCirclesScreenState createState() => _AnimatedCirclesScreenState();
+}
+
+class _AnimatedCirclesScreenState extends State<AnimatedCirclesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _sizeAnimation;
+  late Animation<Color?> _colorAnimation;
+  late Animation<Offset> _positionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _sizeAnimation = Tween<double>(begin: 50.0, end: 150.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _colorAnimation = ColorTween(begin: Colors.blue, end: Colors.red).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _positionAnimation = Tween<Offset>(
+      begin: Offset(0.0, 0.0),
+      end: Offset(0.5, 0.5),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Advanced Animation Example'),
+      ),
+      body: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Stack(
+                children: [
+                  Positioned(
+                    top: 100,
+                    left: 100,
+                    child: SlideTransition(
+                      position: _positionAnimation,
+                      child: ScaleTransition(
+                        scale: _sizeAnimation,
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: _colorAnimation.value,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 300,
+                    left: 200,
+                    child: SlideTransition(
+                      position: _positionAnimation,
+                      child: ScaleTransition(
+                        scale: _sizeAnimation,
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: _colorAnimation.value?.withOpacity(0.5),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      
+      ),
+    
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
